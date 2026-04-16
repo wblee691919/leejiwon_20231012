cb5d54f13f59a63e1ba21a9c81db9aa28a17e3de
Lee Jiwon <wblee691919@duksung.ac.kr>
2026-04-16 17:29:10 +0900

    260416

--- sketch.js
+++ sketch.js
@@ -5,7 +5,13 @@ function setup() {
 function draw() {
   background(220);
   fill(255, 0, 0);
+  ellipse(200, 200, 250, 50);
+  fill(255, 255, 0);
+  ellipse(200, 200, 150, 50);
+  fill(0);
   ellipse(200, 200, 50, 50);
-  //commit?
   
+  
+  //commit?
+
 }
